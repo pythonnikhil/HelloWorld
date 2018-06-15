12ed8a371060fcaf24caf9709cda37e93249161e
yellownikhil <nikhil.choudhari1@gmail.com>
2018-06-15 21:38:13 +0530

    Changes to Factorial file

--- Factorial.java
+++ Factorial.java
@@ -1,14 +1,14 @@
 public class Factorial
 {
 	public static void main(String[] args)
-	{	final int NUM_FACTS = 100;
+	{	final int NUM_FACTS = 18;
 		for(int i = 0; i < NUM_FACTS; i++)
 			System.out.println( i + "! is " + factorial(i));
 	}
 	
 	public static int factorial(int n)
 	{	int result = 1;
-		for(int i = 2; i <= n; i++)
+		for(int i = 3; i <= n; i++)
 			result *= i;
 		return result;
 	}
